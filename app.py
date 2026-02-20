f398c1a249f32d4d29b2b67a927ccf40a9c8aa30
Prasanth00msd <prasanthm0018@gmail.com>
2026-02-20 14:45:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+app = FastAPI()
+
+model = joblib.load("model.pkl")
+
+class IrisInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+@app.get("/")
+def home():
+    return {"message": "Iris ML Model API is running"}
+
+@app.post("/predict")
+def predict(data: IrisInput):
+    features = [[
+        data.sepal_length,
+        data.sepal_width,
+        data.petal_length,
+        data.petal_width
+    ]]
+    prediction = model.predict(features)
+    return {"prediction": prediction[0]}
